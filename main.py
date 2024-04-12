d6aa1a41fc8521d3eb4707bdeb06c41306279159
Rutesh <51776721+Rutesh1902@users.noreply.github.com>
2024-04-12 18:44:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from typing import Optional
+app = FastAPI()
+
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f'{limit} published blog from the db'}
+    else:
+        return {"data": f'{limit} blog from db'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {"data":"all unpublished data"}
+    
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    return {"data": {"1","2"}}
